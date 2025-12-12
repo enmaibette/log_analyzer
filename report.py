93716c6895cc90a14669a62383d21454b7ed24e0
mikiaspaulos3-cell <mikiaspaulos3@gmail.com>
2025-12-12 02:35:24 +0100

    report object list

--- report.py
+++ report.py
@@ -3,13 +3,51 @@ class Report:
         self.suspicious_entries = suspicious_entries
 
     def short_report(self):
-        #total lines processed
-        # number of lines that contain an IP address
-        # total number of failure events
-        # number of suspicious IPs and a list of them
-        pass
+        total_lines=len(self.suspicious_entries)
+        ip_lines = sum(1 for e in self.suspicious_entries if e.ipAddress)
+        failures = sum(1 for e in self.suspicious_entries if e.failed)
+        suspicious_ip=list({e.ipAddress for e in self.suspicious_entries if e.ipAddress})
+        
+        return {
+            "total_lines":total_lines,
+            "ip_lines":ip_lines,
+            "suspicious_ip_count":len(suspicious_ip),
+            "suspicious_ip":suspicious_ip,
+            
+        }
+    
+                
+
+
+        
 
     def detailed_report(self):
+        ip_stats={}
+        for e in self.suspicious_entries:
+            ip=e.ipAddress
+            if not ip:
+                continue
+            if ip not in ip_stats:
+                ip_stats[ip]={"failure":0,"total":0,"app":{}}
+            ip_stats[ip]["total"]+=1
+            if e.failed:
+                ip_stats[ip]["failure"]+=1
+                app=e.applicationName
+                ip_stats[ip]["application"][app]=ip_stats[ip]["application"].get(app,0)+1
+        report ={}
+        for ip, stats in ip_stats.items():
+            failure_rate=stats["failure"]/stats["total"] if stats["total"] else 0
+            report[ip]={
+                "failure":stats["failure"],
+                "total_request":stats["total"],
+                "failure_rate":round(failure_rate,2),
+                "application":stats["application"],
+                "reason":"high failure rate" if failure_rate>0.5 else "Moderate activity"
+             }
+            return report
+            
+                
+
         #suspicious IPs
         # their statistics (number of failures / total requests)
         # a short explanation of why they are suspicious (e.g. more than x
@@ -19,4 +57,4 @@ class Report:
         # Extra information about the failure
         # Number of failures by suspicious IP
         # Etc.
-        pass
+        
